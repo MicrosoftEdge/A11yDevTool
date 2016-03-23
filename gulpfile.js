a81cc01baa42d99c7d7860abf43b24fe19793ae6
Andy Sterland <andster@microsoft.com>
2016-03-23 13:51:01 -0700

    Adding build step for resource strings

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp =  require('gulp');
-var exec = require('gulp-exec');
+var gulp = require('gulp');
 var fs = require('fs');
 var builder = require('xmlbuilder');
+
+var outputFileName = 'accessibilityTool.strings'; 
+var startingIdNumber = 1000;  
 
 gulp.task('buildStrings', function(){    
     /*
@@ -12,28 +14,38 @@ gulp.task('buildStrings', function(){
         <comment></comment>
     </string>  
     */
-    fs.readFile('PropertyMapping.json', 'utf8', function (err,data) {
-        var items = JSON.parse(data);
-        var outputPath = 'dist';
-        var outputFileName = 'accessibilityTool.strings'; 
-        var startingIdNumber = 1000;  
-        var jsonA11yProps = [];  
+    fs.readFile('PropertyMapping.json', 'utf8', function (err, data) {
+        var jsonA11yProps = JSON.parse(data);
+        var jsonA11yPropsKeys = Object.keys(jsonA11yProps);
         
-        var xmlDoc = builder.create('resources');
         var id = startingIdNumber;
         
-        jsonA11yProps.forEach(function(items){
-            id++;
-            var name = 'a11y_' + item.displayName + '_tooltip';
-            var stringItem = xmlDoc.ele('string', {
-                'id': id,
-                'name': name
-            },);
-            stringItem.ele('value', {},  item.description);
-            stringItem.ele('comment', {},  '');
-            stringItem.end({ pretty: true});
-        });    
+        var rootEl  = builder.create('resources');
+        jsonA11yPropsKeys.forEach(function(key){
+            var item = jsonA11yProps[key];
+            var name = 'a11y_' + key + '_tooltip';      
             
-        console.log(xmlDoc);    
+            var stringObject = {
+                'string': {
+                    '@id': id,
+                    '@name': name,                   
+                    'value': item.description,
+                    'comment': ''
+                }
+            };
+            
+            rootEl.ele(stringObject);
+            
+            id++;          
+        });    
+        rootEl.end({ pretty: true});
+        
+        fs.writeFile(outputFileName, rootEl, function(err){
+            if(err) {
+                return console.log(err);
+            }
+            
+            console.log("Saved resource strings to file.");
+        }); 
     });
 });
